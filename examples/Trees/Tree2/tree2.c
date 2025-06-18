fe8830f3c5e6dc77098eb95ecc9120fa718cb637
NoctemLeges <satadrumemini@gmail.com>
2025-06-19 02:45:46 +0530

    Added examples for Multiple Rules

---
+++ examples/Trees/Tree2/tree2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../../includes/rule.h"
+#include "../../../includes/word.h"
+#include "../../../includes/parseconf.h"
+#include "../../../includes/strexp.h"
+#include "../../../includes/render.h"
+
+int main(){
+    
+    FILE* fp = fopen("tree2_conf","r");
+    if(fp==NULL){
+        perror("Error opening tree2_conf");
+        exit(1);
+    }
+    int NumRules;
+    Word* Axiom = (Word*)malloc(sizeof(Word));
+    initArr(Axiom,50);
+    int Delta;
+    int Length;
+    parseNumRules(&NumRules,fp);
+    Rule* rules = (Rule*)malloc(sizeof(Rule)*NumRules);
+    parseConfig(Axiom,rules,&Length,&Delta,fp);
+    //Expand the Axiom with the Rules
+    Word* string = (Word*)malloc(sizeof(Word));
+    initArr(string,50);
+    string = strexp(Axiom,rules,NumRules,Length);
+    drawLSystem(string,Delta,5.0,"tree2.svg");
+}
