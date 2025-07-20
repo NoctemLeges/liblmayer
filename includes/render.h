ad296ab371438f41bd3bfcc5e110ac345ad10df8
NoctemLeges <satadrumemini@gmail.com>
2025-07-21 02:26:10 +0530

    Added Branch Depth Tracking

--- includes/render.h
+++ includes/render.h
@@ -3,5 +3,5 @@
 #include <stdio.h>
 #include "rule.h"
 #include "word.h"
-void drawLSystem(Word* instructions, double angleDelta, double step, const char* filename);
+void drawLSystem(Word* instructions, double angleDelta, double step, const char* filename,int isTree);
 #endif
--- src/render.c
+++ src/render.c
@@ -8,124 +8,280 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+#ifndef THRESHOLD
+#define THRESHOLD 3
+#endif
 
-typedef struct {
+typedef struct
+{
     double x, y, angle;
 } TurtleState;
 
-typedef struct {
-    TurtleState* data;
+typedef struct
+{
+    TurtleState *data;
     int top;
     int capacity;
 } Stack;
 
-void initStack(Stack* s, int capacity) {
-    s->data = (TurtleState*)malloc(sizeof(TurtleState) * capacity);
+void initStack(Stack *s, int capacity)
+{
+    s->data = (TurtleState *)malloc(sizeof(TurtleState) * capacity);
     s->top = -1;
     s->capacity = capacity;
 }
 
-void push(Stack* s, TurtleState value) {
-    if (s->top + 1 >= s->capacity) {
+void push(Stack *s, TurtleState value)
+{
+    if (s->top + 1 >= s->capacity)
+    {
         s->capacity *= 2;
-        s->data = (TurtleState*)realloc(s->data, sizeof(TurtleState) * s->capacity);
+        s->data = (TurtleState *)realloc(s->data, sizeof(TurtleState) * s->capacity);
     }
     s->data[++s->top] = value;
 }
 
-TurtleState pop(Stack* s) {
-    if (s->top >= 0) {
+TurtleState pop(Stack *s)
+{
+    if (s->top >= 0)
+    {
         return s->data[s->top--];
     }
     TurtleState empty = {0, 0, 0};
     return empty;
 }
 
-void freeStack(Stack* s) {
+void freeStack(Stack *s)
+{
     free(s->data);
 }
 
-void drawLSystem(Word* instructions, double angleDelta, double step, const char* filename) {
+void drawLSystem(Word *instructions, double angleDelta, double step, const char *filename, int isTree)
+{
     double x = 0.0, y = 0.0, angle = 90.0;
     double minX = x, minY = y, maxX = x, maxY = y;
 
     Stack stack;
     initStack(&stack, 100);
-    
-    for (size_t i = 0; i < instructions->currSize; i++) {
-        char c = instructions->data[i];
-        if (isalpha(c)) {
-            double newX = x + step * cos(angle * M_PI / 180.0);
-            double newY = y + step * sin(angle * M_PI / 180.0);
-            if (newX < minX) minX = newX;
-            if (newX > maxX) maxX = newX;
-            if (newY < minY) minY = newY;
-            if (newY > maxY) maxY = newY;
-            x = newX;
-            y = newY;
-        } else if (c == '+') {
-            angle += angleDelta;
-        } else if (c == '-') {
-            angle -= angleDelta;
-        } else if (c == '[') {
-            TurtleState current = {x, y, angle};
-            push(&stack, current);
-        } else if (c == ']') {
-            TurtleState state = pop(&stack);
-            x = state.x;
-            y = state.y;
-            angle = state.angle;
+    if (!isTree)
+    {
+        for (size_t i = 0; i < instructions->currSize; i++)
+        {
+            char c = instructions->data[i];
+            if (isalpha(c))
+            {
+                double newX = x + step * cos(angle * M_PI / 180.0);
+                double newY = y + step * sin(angle * M_PI / 180.0);
+                if (newX < minX)
+                    minX = newX;
+                if (newX > maxX)
+                    maxX = newX;
+                if (newY < minY)
+                    minY = newY;
+                if (newY > maxY)
+                    maxY = newY;
+                x = newX;
+                y = newY;
+            }
+            else if (c == '+')
+            {
+                angle += angleDelta;
+            }
+            else if (c == '-')
+            {
+                angle -= angleDelta;
+            }
+            else if (c == '[')
+            {
+                TurtleState current = {x, y, angle};
+                push(&stack, current);
+            }
+            else if (c == ']')
+            {
+                TurtleState state = pop(&stack);
+                x = state.x;
+                y = state.y;
+                angle = state.angle;
+            }
         }
-    }
 
-    double width = maxX - minX + 10.0;
-    double height = maxY - minY + 10.0;
-    double offsetX = -minX + 5.0;
-    double offsetY = -minY + 5.0;
+        double width = maxX - minX + 10.0;
+        double height = maxY - minY + 10.0;
+        double offsetX = -minX + 5.0;
+        double offsetY = -minY + 5.0;
 
-    FILE* svg = fopen(filename, "w");
-    if (!svg) {
-        perror("Error opening SVG file");
+        FILE *svg = fopen(filename, "w");
+        if (!svg)
+        {
+            perror("Error opening SVG file");
+            freeStack(&stack);
+            return;
+        }
+
+        fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%f\" height=\"%f\" viewBox=\"0 0 %f %f\">\n",
+                width, height, width, height);
+        fprintf(svg, "<rect width=\"100%%\" height=\"100%%\" fill=\"black\" />\n");
+        fprintf(svg, "<g stroke=\"white\" stroke-width=\"1\" fill=\"none\">\n");
+
+        x = 0.0;
+        y = 0.0;
+        angle = 90.0;
+        initStack(&stack, 100);
+
+        for (size_t i = 0; i < instructions->currSize; i++)
+        {
+            char c = instructions->data[i];
+            if (isalpha(c))
+            {
+                double newX = x + step * cos(angle * M_PI / 180.0);
+                double newY = y + step * sin(angle * M_PI / 180.0);
+                fprintf(svg, "<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" />\n",
+                        x + offsetX, height - (y + offsetY),
+                        newX + offsetX, height - (newY + offsetY));
+                x = newX;
+                y = newY;
+            }
+            else if (c == '+')
+            {
+                angle += angleDelta;
+            }
+            else if (c == '-')
+            {
+                angle -= angleDelta;
+            }
+            else if (c == '[')
+            {
+                TurtleState current = {x, y, angle};
+                push(&stack, current);
+            }
+            else if (c == ']')
+            {
+                TurtleState state = pop(&stack);
+                x = state.x;
+                y = state.y;
+                angle = state.angle;
+            }
+        }
+        fprintf(svg, "</g>\n</svg>\n");
+        fclose(svg);
         freeStack(&stack);
-        return;
     }
 
-    fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%f\" height=\"%f\" viewBox=\"0 0 %f %f\">\n",
-            width, height, width, height);
-    fprintf(svg, "<rect width=\"100%%\" height=\"100%%\" fill=\"black\" />\n");
-    fprintf(svg, "<g stroke=\"white\" stroke-width=\"1\" fill=\"none\">\n");
-
-    x = 0.0;
-    y = 0.0;
-    angle = 90.0;
-    initStack(&stack, 100);
-
-    for (size_t i = 0; i < instructions->currSize; i++) {
-        char c = instructions->data[i];
-        if (isalpha(c)) {
-            double newX = x + step * cos(angle * M_PI / 180.0);
-            double newY = y + step * sin(angle * M_PI / 180.0);
-            fprintf(svg, "<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" />\n",
-                    x + offsetX, height - (y + offsetY),
-                    newX + offsetX, height - (newY + offsetY));
-            x = newX;
-            y = newY;
-        } else if (c == '+') {
-            angle += angleDelta;
-        } else if (c == '-') {
-            angle -= angleDelta;
-        } else if (c == '[') {
-            TurtleState current = {x, y, angle};
-            push(&stack, current);
-        } else if (c == ']') {
-            TurtleState state = pop(&stack);
-            x = state.x;
-            y = state.y;
-            angle = state.angle;
+    if(isTree){
+        int branchNo = 0;
+        for (size_t i = 0; i < instructions->currSize; i++)
+        {
+            char c = instructions->data[i];
+            if (isalpha(c))
+            {
+                double newX = x + step * cos(angle * M_PI / 180.0);
+                double newY = y + step * sin(angle * M_PI / 180.0);
+                if (newX < minX)
+                    minX = newX;
+                if (newX > maxX)
+                    maxX = newX;
+                if (newY < minY)
+                    minY = newY;
+                if (newY > maxY)
+                    maxY = newY;
+                x = newX;
+                y = newY;
+            }
+            else if (c == '+')
+            {
+                angle += angleDelta;
+            }
+            else if (c == '-')
+            {
+                angle -= angleDelta;
+            }
+            else if (c == '[')
+            {
+                TurtleState current = {x, y, angle};
+                push(&stack, current);
+            }
+            else if (c == ']')
+            {
+                TurtleState state = pop(&stack);
+                x = state.x;
+                y = state.y;
+                angle = state.angle;
+            }
         }
+
+        double width = maxX - minX + 10.0;
+        double height = maxY - minY + 10.0;
+        double offsetX = -minX + 5.0;
+        double offsetY = -minY + 5.0;
+
+        FILE *svg = fopen(filename, "w");
+        if (!svg)
+        {
+            perror("Error opening SVG file");
+            freeStack(&stack);
+            return;
+        }
+
+        fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%f\" height=\"%f\" viewBox=\"0 0 %f %f\">\n",
+                width, height, width, height);
+        fprintf(svg, "<rect width=\"100%%\" height=\"100%%\" fill=\"white\" />\n");
+        fprintf(svg, "<g stroke=\"white\" stroke-width=\"1\" fill=\"none\">\n");
+
+        x = 0.0;
+        y = 0.0;
+        angle = 90.0;
+        initStack(&stack, 100);
+
+        for (size_t i = 0; i < instructions->currSize; i++)
+        {
+            char c = instructions->data[i];
+            if(c=='[') branchNo++;
+            if(c==']') branchNo--;
+            if (isalpha(c) && branchNo<THRESHOLD)
+            {
+                double newX = x + step * cos(angle * M_PI / 180.0);
+                double newY = y + step * sin(angle * M_PI / 180.0);
+                fprintf(svg, "<line stroke=\"brown\" stroke-width=\"3\" x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" />\n",
+                        x + offsetX, height - (y + offsetY),
+                        newX + offsetX, height - (newY + offsetY));
+                x = newX;
+                y = newY;
+            }
+            else if (isalpha(c) && branchNo>=THRESHOLD)
+            {
+                double newX = x + step * cos(angle * M_PI / 180.0);
+                double newY = y + step * sin(angle * M_PI / 180.0);
+                fprintf(svg, "<line stroke=\"green\" x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" />\n",
+                        x + offsetX, height - (y + offsetY),
+                        newX + offsetX, height - (newY + offsetY));
+                x = newX;
+                y = newY;
+            }
+            else if (c == '+')
+            {
+                angle += angleDelta;
+            }
+            else if (c == '-')
+            {
+                angle -= angleDelta;
+            }
+            else if (c == '[')
+            {
+                TurtleState current = {x, y, angle};
+                push(&stack, current);
+            }
+            else if (c == ']')
+            {
+                TurtleState state = pop(&stack);
+                x = state.x;
+                y = state.y;
+                angle = state.angle;
+            }
+        }
+        fprintf(svg, "</g>\n</svg>\n");
+        fclose(svg);
+        freeStack(&stack);
     }
 
-    fprintf(svg, "</g>\n</svg>\n");
-    fclose(svg);
-    freeStack(&stack);
+    
 }
